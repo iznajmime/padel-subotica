5e2343a0fa1aa68aaab00e29e857be8db97db7cf
iznajmime <admin@iznajmi.me>
2025-10-13 16:54:45 +0200

    Update Teren 2 embed code

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -10,10 +10,36 @@ const BookingPage = () => {
   }, []);
 
   useEffect(() => {
-    (async function () {
-      const cal = await getCalApi({"namespace":"teren-2"});
-      cal("ui", {"theme":"light","cssVarsPerTheme":{"light":{"cal-brand":"#0056bf"},"dark":{"cal-brand":"#fafafa"}},"hideEventTypeDetails":false,"layout":"month_view"});
-    })();
+    // Cal element-click embed code for Teren 2
+    (function (C, A, L) { 
+      let p = function (a, ar) { a.q.push(ar); }; 
+      let d = C.document; 
+      C.Cal = C.Cal || function () { 
+        let cal = C.Cal; 
+        let ar = arguments; 
+        if (!cal.loaded) { 
+          cal.ns = {}; 
+          cal.q = cal.q || []; 
+          d.head.appendChild(d.createElement("script")).src = A; 
+          cal.loaded = true; 
+        } 
+        if (ar[0] === L) { 
+          const api = function () { p(api, arguments); }; 
+          const namespace = ar[1]; 
+          api.q = api.q || []; 
+          if(typeof namespace === "string"){
+            cal.ns[namespace] = cal.ns[namespace] || api;
+            p(cal.ns[namespace], ar);
+            p(cal, ["initNamespace", namespace]);
+          } else p(cal, ar); 
+          return;
+        } 
+        p(cal, ar); 
+      }; 
+    })(window, "https://app.cal.com/embed/embed.js", "init");
+    
+    window.Cal("init", "teren-2", {origin:"https://app.cal.com"});
+    window.Cal.ns["teren-2"]("ui", {"theme":"light","cssVarsPerTheme":{"light":{"cal-brand":"#0056bf"},"dark":{"cal-brand":"#fafafa"}},"hideEventTypeDetails":false,"layout":"month_view"});
   }, []);
 
   return (
@@ -39,7 +65,7 @@ const BookingPage = () => {
         
         <button 
           data-cal-namespace="teren-2"
-          data-cal-link="park-klub/teren-2"
+          data-cal-link="padelklubpark/teren-2"
           data-cal-config='{"layout":"month_view","theme":"light"}'
           className="bg-[#0056bf] hover:bg-[#004299] text-white font-semibold rounded-xl shadow-lg transition-colors duration-200 text-xl flex items-center justify-center py-8 h-20"
         >
